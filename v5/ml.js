7e2bdd497a7d2f856f288530d64dda2836b54e21
Sbr-11 <subhrajitbarua.1998@gmail.com>
2025-10-31 00:43:28 +0300

    Delete v5/ml.js

--- v5/ml.js
+++
@@ -1,7 +0,0 @@
-function predictEfficiency(curcumin, temp) {
-  // Simple ML-inspired placeholder function
-  const base = 40;
-  const curcuminEffect = 0.9 * curcumin;
-  const tempEffect = 1.2 * (temp - 20);
-  return Math.min(100, base + curcuminEffect + tempEffect);
-}
